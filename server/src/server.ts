a77eba42df541f5413f8ba8f3d625276d4c8a592
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-09-16 15:36:41 -0300

    Add typescript in the project

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+const PORT = 3000
+const hostname = 'localhost'
+const server = express();
+
+server.get('/', (request, response) => {
+  response.json([
+    {
+      id: 1,
+      name: 'Ester Durans'
+    },
+    {
+      id: 2,
+      name: 'Wellington Durans'
+    }
+  ])
+})
+
+server.listen(PORT, hostname, () => console.log(`Server running at http://${hostname}${PORT}`))

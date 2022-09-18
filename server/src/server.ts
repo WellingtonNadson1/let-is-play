defc75379baeb9257883502299e64d6c8348b8f1
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-09-18 12:23:12 -0300

    Created API routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,17 +3,22 @@ const PORT = 3000
 const hostname = 'localhost'
 const server = express();
 
-server.get('/', (request, response) => {
-  response.json([
-    {
-      id: 1,
-      name: 'Ester Durans'
-    },
-    {
-      id: 2,
-      name: 'Wellington Durans'
-    }
-  ])
+// Query Params
+server.get('/games', (request, response) => {
+  response.json([])
+})
+
+server.post('/ads', (request, response) => {
+  response.status(201).json([])
+})
+
+// Route Params
+server.get('/games/:id/ads', (request, response) => {
+  response.json([])
+})
+
+server.get('/ads/:id/discord', (request, response) => {
+  response.json([])
 })
 
 server.listen(PORT, hostname, () => console.log(`Server running at http://${hostname}${PORT}`))
